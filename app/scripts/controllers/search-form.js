c22a58d4d65525bd12eb04e6ffb607f0d6daad44
Benjamin Doherty <bjd@bangpound.org>
2016-12-06 00:15:19 -0600

    remove debug.

--- app/scripts/controllers/search-form.js
+++ app/scripts/controllers/search-form.js
@@ -11,6 +11,5 @@ angular.module('kal3aSearchApp')
   .controller('SearchFormCtrl', ['searchQuery', function (searchQuery) {
       this.query = searchQuery;
       this.search = function (query) {
-          console.log(query);
       };
   }]);
