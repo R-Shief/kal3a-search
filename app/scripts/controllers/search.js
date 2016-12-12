9b9bb68594652588da3a2ca97d1d11e8a830dcff
Benjamin Doherty <bjd@bangpound.org>
2016-12-11 19:40:12 -0600

    set up headings on aggregation panes.

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -9,4 +9,10 @@ angular.module('kal3aSearchApp')
   .controller('SearchCtrl', ['aggs', function (aggs) {
       'use strict';
       this.aggs = aggs;
+      this.headings = {
+          'authors.name.keyword': 'Author',
+          'categories.term.keyword': 'Tag',
+          'lang': 'Language',
+          'source.title.text.keyword' : 'Source'
+      };
   }]);
